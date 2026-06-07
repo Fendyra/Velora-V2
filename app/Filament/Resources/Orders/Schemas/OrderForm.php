839e56870f3bf0c6f4d305d11955e29f5aca496d
fendyra <fendyrarestu2004@gmail.com>
2026-06-07 20:38:55 +0700

    Fix Filament Section component namespace

--- app/Filament/Resources/Orders/Schemas/OrderForm.php
+++ app/Filament/Resources/Orders/Schemas/OrderForm.php
@@ -5,7 +5,7 @@ namespace App\Filament\Resources\Orders\Schemas;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\Repeater;
-use Filament\Forms\Components\Section;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class OrderForm
